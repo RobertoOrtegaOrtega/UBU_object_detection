dabbc996687a32b0444e781238d79b9b06d6cb3a
Roberto <roo0002@alu.ubu.es>
2017-11-01 22:28:43 +0100

    Deteccion de objetos V.1

---
+++ src/objectDetection.py
@@ -0,0 +1,41 @@
+import cv2
+from matplotlib import pyplot as plt
+
+imagen = cv2.imread('imagen.png')
+
+plt.subplot(221), plt.imshow(imagen), plt.title('Original')
+plt.xticks([]), plt.yticks([])
+
+imagenGris = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
+
+mediana = cv2.blur(imagenGris, (15,15))
+#No funciona con mala luz, o extraña
+#t, imUmb = cv2.threshold(imagenGris, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_TRIANGLE)
+
+imUmb = cv2.adaptiveThreshold (mediana, 255, cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY, 11,2)
+
+_, contours, _ = cv2.findContours(imUmb, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+#dibuja borde
+"""for c in contours:
+    area = cv2.contourArea(c)
+    if area > 1000 and area < 1000000:
+        cv2.drawContours(imagen, [c], 0, (0, 255, 0), 2, cv2.LINE_AA)"""
+
+plt.subplot(222), plt.imshow(imUmb), plt.title('bordes')
+plt.xticks([]), plt.yticks([])
+plt.subplot(223), plt.imshow(imagen), plt.title('rectangulo')
+plt.xticks([]), plt.yticks([])
+
+
+#dibuja recuadro
+for c in contours:
+    area = cv2.contourArea(c)
+    #retocar area minima y maxima posible
+    if area > 1000 and area < 1000000:
+        (x, y, w, h) = cv2.boundingRect(c)
+        cv2.rectangle(imagen, (x, y), (x + w, y + h), (255, 0, 0), 1, cv2.LINE_AA)
+
+plt.subplot(224), plt.imshow(imagen), plt.title('rectangulo')
+plt.xticks([]), plt.yticks([])
+
+plt.show()
