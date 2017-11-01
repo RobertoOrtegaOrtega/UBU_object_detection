9e5f13d51f82ec5c687b3feae5a27f77aae615f4
Roberto <roo0002@alu.ubu.es>
2017-11-01 22:29:29 +0100

    Toma de fotos

--- src/takePhoto.py
+++ src/takePhoto.py
@@ -4,10 +4,9 @@ camara = cv2.VideoCapture(0)
 ret,imagen = camara.read()
 
 while(True):
-    cv2.imshow('img1',imagen)
-    if cv2.waitKey(1) & 0xFF == ord('y'):
-        cv2.imwrite('images/c1.png',imagen)
+    cv2.imshow('imagen',imagen)
+    if cv2.waitKey(0) & 0xFF == ord('n'):
+        cv2.imwrite('imagen.png',imagen)
         cv2.destroyAllWindows()
         break
-
 camara.release()
