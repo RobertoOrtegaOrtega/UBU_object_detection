1b140f295c29690352fe2488d6ebb4836d52fb66
Roberto <roo0002@alu.ubu.es>
2017-11-07 16:17:35 +0100

    compareObject dira que objetos son aun necesrios para el montaje.

---
+++ src/menu.py
@@ -0,0 +1,22 @@
+print('Bienvenido')
+print('¿La pieza que desea supervisar tiene una fase de montaje predefinido?')
+print('1.- Si, tiene unas fases establecidas las cuales son inmutables')
+print('2.- No, la pieze puede ser montada como se desea, siempre que el resultado este bien')
+opcion=0
+while opcion<1 or opcion>2:
+    opcion = input('Introduce la opcion deseada')
+previo='n'
+while previo == 'n' or previo == 'y':
+    print('¿Se conoce con anterioridad las fases de montaje de la pieza o el resultado final?')
+    previo = input('(y/n)',)
+if opcion==1:
+    if previo=='y':
+        print('catalogo de piezas que pueden ser montadas')
+    else:
+        print('montaje de prueba')
+else:
+    if previo == 'y':
+        print('catalogo de piezas que pueden ser montadas')
+    else:
+        print('montaje de prueba')
+
