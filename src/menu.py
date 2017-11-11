9d722f9e25e6f1c2ceda3430ec8f7ccda4a5f50f
Roberto <roo0002@alu.ubu.es>
2017-11-11 21:25:11 +0100

    Mejora del menu de ejecucion

--- src/menu.py
+++ src/menu.py
@@ -1,15 +1,20 @@
+
 #Script principal
+from src.compareObjects2 import compareObjects
+from src.countObjects import countObject
+
 print('Bienvenido')
 print('¿La pieza que desea supervisar tiene una fase de montaje predefinido?')
 print('1.- Si, tiene unas fases establecidas las cuales son inmutables')
 print('2.- No, la pieze puede ser montada como se desea, siempre que el resultado este bien')
 opcion=0
 while opcion<1 or opcion>2:
-    opcion = input('Introduce la opcion deseada')
-previo='n'
-while previo == 'n' or previo == 'y':
+    opcion = int(input('Introduce la opcion deseada'))
+previo='l'
+while previo != 'n' and previo != 'y':
     print('¿Se conoce con anterioridad las fases de montaje de la pieza o el resultado final?')
     previo = input('(y/n)',)
+    print(previo != 'n' or previo != 'y')
 if opcion==1:
     if previo=='y':
         print('catalogo de piezas que pueden ser montadas')
@@ -18,6 +23,22 @@ if opcion==1:
 else:
     if previo == 'y':
         print('catalogo de piezas que pueden ser montadas')
+        #comprobacion estandar de piezas
+        """val=0
+        for i in range(12):
+            print(i)
+            cad='sector'+str(i+1)+'.png'
+            val=val+compareObjects(cad,cad)
+        print(val)"""
+        #ejemplo puzzle
+        val = 0
+        for i in range(12):
+            print(i)
+            cad = 'sector' + str(i + 1) + '.png'
+            val = val + compareObjects(cad, 'puzzleFinal.png')
+        print(val)
     else:
         print('montaje de prueba')
-
+        imagen='faseFinal_3.png'
+        objetos=countObject(imagen)
+        print(objetos)
