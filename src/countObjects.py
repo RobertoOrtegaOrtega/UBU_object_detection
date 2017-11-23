02b9d56aee7b7fe0a77e91c80b4fb88d3428cbbe
Roberto <roo0002@alu.ubu.es>
2017-11-23 19:07:40 +0100

    Reparacion de mal conteo de objetos debido a la no inversion de la foto de objetos

--- src/countObjects.py
+++ src/countObjects.py
@@ -3,6 +3,7 @@ import cv2
 
 def countObject(imagen1,imagen2):
     img1 = cv2.imread(imagen1, 0)
+    img1 = cv2.bitwise_not(img1)
     img2 = cv2.imread(imagen2)
     (_, contornos, _) = cv2.findContours(img1,cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     # dibuja borde
@@ -12,16 +13,14 @@ def countObject(imagen1,imagen2):
         if area > 1000:
             print('-----------------')
             contornoOk=contornoOk+1
-            #rect = cv2.minAreaRect(c)
-            #box = cv2.boxPoints(rect)
-            #box = np.int0(box)
             (x, y, w, h) = cv2.boundingRect(c)
             cv2.rectangle(img2, (x, y), (x + w, y + h), (255, 0, 0), 1, cv2.LINE_AA)
-            #cv2.drawContours(img, [box], 0, (0, 255, 0), 2, cv2.LINE_AA)
-            cv2.imshow('parte',img2[y:y+h,x:x+w])
+            cv2.drawContours(img2, [c], 0, (0, 255, 0), 2, cv2.LINE_AA)
             nombre='objeto'+ str(contornoOk-1) + '.png'
+            cv2.imshow(nombre, img2[y:y + h, x:x + w])
             cv2.imwrite(nombre, img2[y:y+h,x:x+w])
             cv2.waitKey(0)
-
+    cv2.imshow(nombre, img2)
+    cv2.waitKey(0)
     print("He encontrado %d objetos" %contornoOk)
     return contornoOk
