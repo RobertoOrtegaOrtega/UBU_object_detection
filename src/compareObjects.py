8e60da62e2d23e7583b68e64160e3693e5d86b54
Roberto <roo0002@alu.ubu.es>
2017-12-15 20:04:44 +0100

    Eliminación de muestras de imagenes innecesarias

--- src/compareObjects.py
+++ src/compareObjects.py
@@ -8,9 +8,9 @@ def compareObjects(imagen1,imagen2,salida):
 
     imagen_gris = cv2.cvtColor(diferencias, cv2.COLOR_BGR2GRAY)
 
-    cv2.imshow('Imagen1', imagen1)
+    """cv2.imshow('Imagen1', imagen1)
     cv2.imshow('Imagen2', imagen2)
     cv2.imshow('Diferencias detectadas', diferencias)
     cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    cv2.destroyAllWindows()"""
     cv2.imwrite('BaseDatos/'+salida+'.png', diferencias)
