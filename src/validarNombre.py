faf9344133cf7a0ade7964c319d9503f354dcd4d
Roberto <roo0002@alu.ubu.es>
2017-12-18 23:19:34 +0100

    Nuevo sistema de direccionamiento entre ventanas

--- src/validarNombre.py
+++ src/validarNombre.py
@@ -1,29 +1,18 @@
-import tkinter
-
 from src.aleatorioConocido import aleatorioConocido
+from src.aleatorioNuevo import aleatorioNuevo
 from src.secuencialConocido import secuencialConocido
 
 
-def validarNombre(selectorGui,cuadroTexto,misNombres,opcion ):
-    check = cuadroTexto.get()
-    print("Mi check: "+check)
+def validarNombre(selectorGui,nombre,opcion,conocido):
 
-    if check in misNombres:
-        selectorGui.destroy()
-        if opcion==0:
-            secuencialConocido(check,1)
-        else:
-            aleatorioConocido(check)
-    elif len(check)==0:
+    if conocido==1:
         if opcion == 0:
             print("Montaje secuencial")
         else:
-            print("Montaje aleatorio")
-    else:
-        error = tkinter.Tk()
-        error.title("Error")
-        error.configure(background='LightBlue')
-        label1 = tkinter.Label(error, text="ERROR: nombre incorrecto", font=("Helvetica", 16),
-                               bg='LightBlue', anchor="w", justify="left")
-        label1.place(relx=0.5, rely=0.5, anchor="center")
-        error.mainloop()
+            aleatorioNuevo(nombre)
+    else :
+        selectorGui.destroy()
+        if opcion==0:
+            secuencialConocido(nombre,1)
+        else:
+            aleatorioConocido(nombre)
